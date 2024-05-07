41d7ac841913fc6eab1135749e5d00a227fee2bd
Chris Atam <hecanotus@gmail.com>
2024-05-07 11:22:20 +0100

    test

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Set the 'public' directory to serve static files
+app.use(express.static("public"));
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://ris:4Yw6ZhShiYBxMZEI@nodecluster.w2hcndg.mongodb.net/sample_mfix?retryWrites=true&w=majority&appName=NodeCluster"
+);
+
+// Check if MongoDB connected successfully
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+
+  // Start the Express server after successful database connection
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+// Handle MongoDB connection error
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// Routes
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
