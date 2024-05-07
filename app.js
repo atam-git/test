5c5c636d1a0cc311e83bb4ec6945218104135578
Chris Atam <hecanotus@gmail.com>
2024-05-07 11:44:10 +0100

    dir added

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 // EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Set the 'public' directory to serve static files
 app.use(express.static("public"));
